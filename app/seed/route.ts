3f84c52050ee1c50c60e2fe8d3dcc772083495fe
BLACKDRAGON03 <abdullahalsayef17@gmail.com>
2024-09-24 12:40:59 +0600

    shfoihfi

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -102,7 +102,7 @@ async function seedRevenue() {
 }
 
 export async function GET() {
-  
+  // return Response.json({ message: 'Database' })
   try {
     await client.sql`BEGIN`;
     await seedUsers();
